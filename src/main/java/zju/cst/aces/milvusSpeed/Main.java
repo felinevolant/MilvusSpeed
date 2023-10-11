afe15a8bd8cc560881caff38dffd4b7e1a9eae29
funforever <1545858682@qq.com>
2023-10-11 21:34:19 +0800

    add feature

--- src/main/java/zju/cst/aces/milvusSpeed/Main.java
+++ src/main/java/zju/cst/aces/milvusSpeed/Main.java
@@ -1,7 +1,25 @@
-package org.example;
+package zju.cst.aces.milvusSpeed;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length == 0)
+            return;
+        switch (args[0]) {
+            case "insert-data":
+                SolveUtil.buildData(args[1]);
+                System.out.println("insert over");
+            case "rebuild-data":
+                MilvusUtil.rebuild();
+                System.out.println("rebuild over");
+            case "get-data-num":
+                System.out.println(MilvusUtil.getVectorNum());
+            case "flush-data":
+                MilvusUtil.flushData();
+                System.out.println("flush over");
+            case "test-data":
+                SolveUtil.testData();
+                System.out.println("test over");
+
+        }
     }
 }
--- src/main/java/zju/cst/aces/milvusSpeed/SolveUtil.java
+++ src/main/java/zju/cst/aces/milvusSpeed/SolveUtil.java
@@ -6,14 +6,27 @@ import org.openlca.npy.NpyFloatArray;
 
 import java.io.File;
 
-public class BuildData {
+import static java.lang.Math.random;
+
+public class SolveUtil {
     public static void buildData(String loc) {
         File file = new File(loc);
 
         NpyFloatArray npyArray = Npy.read(file).asFloatArray();
         for(int i = 0; i < npyArray.shape()[0]; i++){
-            Array2d.getRow(npyArray, i);
+            float[] vector = Array2d.getRow(npyArray, i);
+            MilvusUtil.insertData(vector);
         }
-        System.out.println(npyArray);
+    }
+    public static void testData() {
+        float[] vector = new float[768];
+        long times = 0;
+        for(int cnt = 0; cnt < 1000; cnt++){
+            for(int i = 0; i < 768; i++){
+                vector[i] = (float) random();
+            }
+            times += MilvusUtil.search(vector);
+        }
+        System.out.println("cnt: 100, avg: " + times / 1000.0);
     }
 }
